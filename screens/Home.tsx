ad67322b9c2239afdc29dd344fed3ff10f14db66
Sofia Howard <howard.sofia@berkeley.edu>
2022-09-22 16:04:18 -0700

    added firebase config and basic navigation

---
+++ screens/Home.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useAuthentication } from "../utils/hooks/useAuthentication";
+import { Button } from "react-native-elements";
+import { getAuth, signOut } from "firebase/auth";
+
+const auth = getAuth();
+
+const HomeScreen = ({ navigation }: any) => {
+  const { user } = useAuthentication();
+
+  return (
+    <View style={styles.container}>
+      <Text>Welcome {user?.email}!</Text>
+
+      <Button
+        title="Sign Out"
+        style={styles.button}
+        onPress={() => signOut(auth)}
+      />
+      <Button
+        title="Job Feed"
+        style={styles.button}
+        onPress={() => navigation.navigate("Feed")}
+      />
+      <Button
+        title="Job post drafting"
+        style={styles.button}
+        onPress={() => navigation.navigate("Draft")}
+      />
+      <Button
+        title="Sign In"
+        style={styles.button}
+        onPress={() => navigation.navigate("Signin")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    marginTop: 10,
+  },
+});
+
+export default HomeScreen;
