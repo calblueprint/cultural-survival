e4ba13f894441ff33ab083388e26faf32488b11e
Sofia Howard <howard.sofia@berkeley.edu>
2022-10-01 17:06:32 -0700

    moved files into src

--- firebase/firestore/users.ts
+++
@@ -1,5 +0,0 @@
-import  { db } from "../firebaseApp";
-import  { collection } from "firebase/firestore";
-import { User } from "../../types/schema";
-
-const userCollection = collection(db, "users");
