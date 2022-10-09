367ad19cbf44eed6b95e7d8273dec01a19fc6419
Alvaro Ortiz <varortz@berkeley.edu>
2022-10-09 15:05:51 -0700

    grant queries in progress

--- src/firebase/firestore/grants.ts
+++ src/firebase/firestore/grants.ts
@@ -2,4 +2,5 @@ import  { db } from "../firebaseApp";
 import  { collection } from "firebase/firestore";
 import { Grant } from "../../types/schema";
 
+
 const grantsCollection = collection(db, "grants");
