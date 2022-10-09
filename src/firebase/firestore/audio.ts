13ffc92d93975c96c041d7458e580c3ea081bf88
emilysunaryo <eadorasunaryo@berkeley.edu>
2022-10-09 15:07:19 -0700

    updated audio backend

--- src/firebase/firestore/audio.ts
+++ src/firebase/firestore/audio.ts
@@ -1,5 +1,44 @@
 import  { db } from "../firebaseApp";
-import  { collection } from "firebase/firestore";
+import  { collection, doc, getDoc, getDocs, addDoc, setDoc, deleteDoc} from "firebase/firestore";
 import { Audio } from "../../types/schema";
 
 const audioCollection = collection(db, "audio");
+
+/* Returns audio based on specific audio ID */
+export const getAudio = async(audio_id : string) : Promise <Audio>  => {
+    const docRef = doc(db, "audio", audio_id)
+    const docSnap = await getDoc(docRef);
+    return await parseAudio(docSnap)
+};
+
+export const getAllAudio = async(): Promise <Audio[]> => {
+    const itemsRef = audioCollection;
+    const promises: Promise<Audio>[] = [];
+    const docSnap = await getDocs(itemsRef);
+    docSnap.forEach((audio) => {
+        promises.push(parseAudio(audio))
+    })
+    const audios = await Promise.all(promises);
+    return audios
+}
+
+
+export const addAudio = async (audio: Audio) : Promise<void> => {
+    const itemsRef = collection(db, "audio");
+    await addDoc(itemsref, audio)
+}
+
+
+
+const parseAudio = async (doc) => {
+    const audio_id = doc.id.toString();
+    const data = doc.data();
+    const audio = {
+        audio_id: data.audio_id,
+        audio_file : data.audio_file,
+        authors: data.authors,
+        description : data.description, 
+        title : data.title
+    };
+    return audio as Audio; 
+};
--- src/screens/Audio.tsx
+++ src/screens/Audio.tsx
@@ -1,12 +1,42 @@
 import { StyleSheet, Text, View } from "react-native";
 import { useAuthentication } from "../utils/hooks/useAuthentication";
 import { Button } from "react-native-elements";
+import {useEffect, useState} from 'react';
 import { getAuth, signOut } from "firebase/auth";
+import { getAudio } from "../firebase/firestore/audio.ts";
+import  { db } from "../firebaseApp";
+import  { collection, doc } from "firebase/firestore";
+
+
 
 const auth = getAuth();
 
+
+
 const AudioScreen = ({ navigation }: any) => {
   const { user } = useAuthentication();
+  const [audio, setAudio] = useState([]);
+
+// useEffect(() => {
+//   const getAudio = async() => {
+//   const data = await getAudio('c2t1XX4BwD3hhberGdQH');
+//   console.log(data);
+//   // setAudio(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+//   };
+
+//   getAudio();
+// }, []);
+
+
+useEffect(() => {
+  getAudio('c2t1XX4BwD3hhberGdQH').then((audio) => {
+    setAudio(audio);
+    console.log(audio.authors);
+  })
+
+  getAudio();
+}, []);
+
 
   return (
     <View style={styles.container}>
@@ -16,6 +46,14 @@ const AudioScreen = ({ navigation }: any) => {
         style={styles.button}
         onPress={() => navigation.navigate("Home")}
       />
+      <Text>
+       <div> 
+       <h1> Authors: {audio.authors} </h1>
+       <h1> Description : {audio.description} </h1>
+       <h1> Title: {audio.title} </h1> 
+       <h1> File: {audio.file} </h1>
+       </div>
+      </Text> 
     </View>
   );
 };
