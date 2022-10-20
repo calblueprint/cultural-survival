c6cc3a4327cdda4bd93548e272276cbee8b97d14
miha-bhaskaran <athmiha@berkeley.edu>
2022-10-19 18:40:57 -0700

    done with implementing i18sense

--- App.tsx
+++ App.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import './src/firebase/firebaseApp';
 import RootNavigation from './src/navigation';
 
+
 export default function App() {
+  
+
   return (
       <RootNavigation />
   );
--- src/screens/Translation.tsx
+++ src/screens/Translation.tsx
@@ -1,27 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableHighlightBase, View } from "react-native";
 import { useAuthentication } from "../utils/hooks/useAuthentication";
 import { Button } from "react-native-elements";
 import { getAuth, signOut } from "firebase/auth";
+import { useTranslation } from "react-i18next";
+import { namespaces } from "../i18n/i18n.constants";
+import "../i18n/i18n";
+
 
 const auth = getAuth();
 
+
 {
-  "compilerOptions": {
-    // ... other settings
-    "allowJs": true
+  "compilerOptions"; {
+    "allowJs"; true
   }
 }
-
+var lang = 'es'
 
 const TranslationScreen = ({ navigation }: any) => {
   const { user } = useAuthentication();
+  const { i18n, t } = useTranslation(namespaces.pages.hello);
+  function handleClick(lang: string) {
+    i18n.changeLanguage(lang);
+
+  }
+  
+  function toggle(lg: string) {
+    console.log(lang)
+    lang = (lg == 'en' ? 'es' : 'en')
+    console.log(lang)
+    return lang
+
+  }
+
 
   return (
     <View style={styles.container}>
-      <Text>TranslationScreen</Text>
-      <Text>Welcome</Text>
-      <Text>Radio</Text>
-      <Text>Grants</Text>
+      <text>{t("welcome")}</text>
+      <text>{t("radio")}</text>
+      <text>{t("grants")}</text>
+      <Button 
+      title="English"
+      {...t("buttons.ok", { ns: namespaces.common })}
+      onPress={() => handleClick('en')}
+      />
+      <Button 
+      title="Spanish"
+      {...t("buttons.ok", { ns: namespaces.common })}
+      onPress={() => handleClick('es')}
+      />
+      <Button 
+      title="toggle"
+      {...t("buttons.ok", { ns: namespaces.common })}
+      onPress={() => handleClick(toggle(lang))}
+      />
       <Button
         title="Back"
         style={styles.button}
