49b786f05b6ab8b72b10fa4449094319413b1641
Sofia Howard <howard.sofia@berkeley.edu>
2022-10-27 16:46:13 -0700

    updated codebase to use mongodb

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,10 +1,10 @@
-import { useAuthentication } from '../utils/hooks/useAuthentication';
-import UserStack from './userStack';
-import AuthStack from './authStack';
+// import { useAuthentication } from '../utils/hooks/useAuthentication';
+import UserStack from "./userStack";
+import AuthStack from "./authStack";
 
 export default function RootNavigation() {
-  const { user } = useAuthentication();
+  // const { user } = useAuthentication();
 
   // TODO: switch this order after authentication has been properly implemented
-  return user ? <AuthStack /> : <UserStack /> ; 
+  return <UserStack />;
 }
