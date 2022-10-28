970b0e3dac1904969ac30cc26df588ba7b4f39d4
miha-bhaskaran <athmiha@berkeley.edu>
2022-10-27 17:32:45 -0700

    Saving everything after mph

--- src/screens/TranslationScreen/Translation.tsx
+++ src/screens/TranslationScreen/Translation.tsx
@@ -6,14 +6,37 @@ import { useTranslation } from "react-i18next";
 import { namespaces } from "../../i18n/i18n.constants";
 import "../../i18n/i18n";
 
+
 import ViewContainer from "../../components/ViewContainer";
 import RectButton from "../../components/RectButton";
 import globalStyles from "../../globalStyles";
+import React, { useState, useEffect } from "react";
+
+
+import {
+  CREDENTIALS
+} from "@env";
+
 
 
 
 const auth = getAuth();
 
+const {Translate} = require('@google-cloud/translate').v2;
+require('dotenv').config();
+
+// Your credentials
+//const CREDENTIALS = JSON.parse(TEST);
+//const CREDENTIALS = process.env.CREDENTIALS;
+const CRED = JSON.parse(CREDENTIALS);
+
+
+
+// Configuration for the client
+const translate = new Translate({
+  credentials: CRED,
+  projectId: CRED.project_id
+});
 
 {
   "compilerOptions"; {
@@ -25,6 +48,29 @@ var lang = 'es'
 const TranslationScreen = ({ navigation }: any) => {
   const { user } = useAuthentication();
   const { i18n, t } = useTranslation(namespaces.pages.hello);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    //get all ur grants from mongodb
+
+    // go through all your grants and translate all the text
+
+    // put these in a state variable
+    translateText("potato", "es").then((text) => {
+      console.log(text);
+    });
+  }, [])
+
+  const translateText = async (text, targetLanguage) => {
+    try {
+        let [response] = await translate.translate(text, targetLanguage);
+        return response;
+    } catch (error) {
+        console.log(`Error at translateText --> ${error}`);
+        return 0;
+    }
+  };
+
   function handleClick(lang: string) {
     i18n.changeLanguage(lang);
 
@@ -44,6 +90,26 @@ const TranslationScreen = ({ navigation }: any) => {
       <Text style={globalStyles.h2}>{t("welcome")}</Text>
       <Text style={globalStyles.h2}>{t("radio")}</Text>
       <Text style={globalStyles.h2}>{t("grants")}</Text>
+      <Text style={globalStyles.h4}>
+        {/* {
+        translateText("Please let this work", "es")
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) =>{
+            console.log(res);
+            
+          })
+        }; */}
+        </Text>
+      
+      
+
+     
+     
+
+
+    
       <RectButton 
       text="English"
       {...t("buttons.ok", { ns: namespaces.common })}
