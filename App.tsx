cea02e23c993ef5754049e426787ad1585711a30
Sofia Howard <howard.sofia@berkeley.edu>
2022-11-03 17:55:53 -0700

    added expo dev client to app.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import RootNavigation from "./src/navigation";
 import { AppProvider, UserProvider, useUser } from "@realm/react";
 import { View, ActivityIndicator } from "react-native";
 import { AuthProvider } from "./src/utils/providers/AuthProviders";
+import "expo-dev-client";
 
 export default function App() {
   return (
